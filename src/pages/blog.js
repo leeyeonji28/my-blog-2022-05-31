3333c9d044f83a651deee62de87885ab76334f15
leeyeonji28 <yeon102800@gmail.com>
2022-06-01 21:22:23 +0900

    블로그 내용 표시

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,19 @@
 import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 
 function BlogPage() {
   const data = useStaticQuery(graphql`
     query {
-      allFile {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         nodes {
-          name
+          frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
+          }
+          id
+          body
         }
       }
     }
@@ -17,11 +23,13 @@ function BlogPage() {
     <Layout pageTitle="My Blog Posts">
       <p>My cool posts will go in here</p>
 
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 }
