9eca96c67774f932013ae2ea6c33db4e009cc951
leeyeonji28 <yeon102800@gmail.com>
2022-06-01 11:47:36 +0900

    StaticImage 컴포넌트로 로컬이미지 보여주기

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,15 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial</p>
+      // 웹 링크로 이미지 불러오기
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      // 다운받은 이미지 불러오기
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/puppy.jpeg"
       />
     </Layout>
   );
