e66752f93533641eb46c2138a822d6bd752cfc62
leeyeonji28 <yeon102800@gmail.com>
2022-06-01 13:14:13 +0900

    사이트 제목 꾸미기

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
-    description: "이 사이트는 개발자 이연지의 개발일지가 담겨있습니다.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
