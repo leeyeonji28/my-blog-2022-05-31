38b24246a264af8e5e51c5a040cf11b8a7a8a84c
leeyeonji28 <yeon102800@gmail.com>
2022-06-01 12:27:37 +0900

    그래프QL로 데이터 가져와서 레이아웃에 표시

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
+    description: "이 사이트는 개발자 이연지의 개발일지가 담겨있습니다.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
