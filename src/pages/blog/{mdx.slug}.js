108a14c1fc88b2b98dbc643b4b1c2cb928bd6a6e
leeyeonji28 <yeon102800@gmail.com>
2022-06-02 13:21:39 +0900

    이미지 없는 글에 대한 처리

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -5,18 +5,33 @@ import React from "react";
 import Layout from "../../components/layout";
 
 const BlogPost = ({ data }) => {
-  const image = getImage(data.mdx.frontmatter.hero_image);
+  // && and연산자를 사용해서 data.mdx.frontmatter.hero_image에 값이 없을 시(null) getImage실행을 안하도록 함.
+  const image =
+    data.mdx.frontmatter.hero_image &&
+    getImage(data.mdx.frontmatter.hero_image);
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
-      <p>
-        Photo Credit:{" "}
-        <a target="_blank" href={data.mdx.frontmatter.hero_image_credit_link}>
-          {data.mdx.frontmatter.hero_image_credit_text}
-        </a>
-      </p>
+      {image && (
+        // <></> => GatsbyImage와 p를 묶어줌
+        <>
+          <GatsbyImage
+            image={image}
+            alt={data.mdx.frontmatter.hero_image_alt}
+          />
+          <p>
+            Photo Credit:{" "}
+            <a
+              target="_blank"
+              href={data.mdx.frontmatter.hero_image_credit_link}
+            >
+              {data.mdx.frontmatter.hero_image_credit_text}
+            </a>
+          </p>
+        </>
+      )}
+
       <hr />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
